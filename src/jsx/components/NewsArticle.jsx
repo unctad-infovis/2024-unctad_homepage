362f7e45c1868c2b89afe0a1005b468291df3d19
Teemo Tebest <teemo.tebest@gmail.com>
2024-04-03 12:35:02 +0200

    Intered

--- src/jsx/components/NewsArticle.jsx
+++ src/jsx/components/NewsArticle.jsx
@@ -102,6 +102,105 @@ function App() {
           , aimed at halting and reversing biodiversity loss by 2030.
         </p>
       </div>
+      <div style={{ marginTop: '200px' }} />
+      <div className="title_container inter">
+        <h2>Advancing shared prosperity through biodiversity-friendly trade</h2>
+        <h4>27 March 2024</h4>
+        <h3>With the right policy and governance approach, sustainable trade can flourish and, in turn, nourish the very biodiversity on which it relies.</h3>
+        <figure>
+          <img src="https://dummyimage.com/1600x900/000/fff" alt="Main" />
+          <figcaption>
+            <strong>© Shutterstock/Somchai_Stock</strong>
+            {' '}
+            | A coffee farmer in Gia Lai, Vietnam. Sustainable trade helps conserve natural resources and empower communities.
+          </figcaption>
+        </figure>
+      </div>
+      <div className="content_container inter">
+        <ul>
+          <li>Biodiversity is declining at an alarming rate, with one million species at risk of extinction.</li>
+          <li>Trade can help protect biodiversity by promoting sustainability across the entire value chain.</li>
+          <li>Implementing “BioTrade” principles and criteria is key to safeguarding economic and social benefits on a healthy planet.</li>
+        </ul>
+        <p>With one million species currently at risk of extinction, the state of global biodiversity loss spells trouble for nature and economies.</p>
+        <p>The impact of losing bees and other wild pollinators, fisheries and forestry – just a fraction of natural resources at risk – could reduce global GDP by an estimated $2.7 trillion annually by 2030, according to World Bank simulations.</p>
+        <p>Kicking off the 7th BioTrade Congress in Geneva, UNCTAD and partners gave a renewed push for stronger trade policies and governance to help tackle the biodiversity crisis.</p>
+        <p>“Let us trade, yes, but let us trade in a way that enriches our forests, revitalizes our oceans and purifies our air,” UNCTAD Secretary-General Rebeca Grynspan said as she opened the meeting on 25 March.</p>
+        <p>“Let us ‘BioTrade’, with the right support for that to happen,” the secretary-general added.</p>
+        <h4>Why ‘BioTrade’ matters</h4>
+        <p>
+          Valued at $3.7 trillion, products with a biological origin represented 17% of global exports in 2021, according to the Trade and Biodiversity (
+          <a href="https://unctadstat.unctad.org/EN/Biotrade.html">TraBio</a>
+          ) statistical tool, UNCTAD’s
+          {' '}
+          <a href="https://unctadstat.unctad.org/datacentre/">database</a>
+          {' '}
+          that measures the international trade of
+          {' '}
+          <em>biodiversity</em>
+          -based products.
+        </p>
+        <p>The economic stakes are even higher for low-income economies, with the share often surpassing 40% of their exports during the past decade.</p>
+        <p>
+          Since launching the
+          {' '}
+          <a href="/topic/trade-and-environment/biotrade">BioTrade Initiative</a>
+          {' '}
+          in 1996, UNCTAD has supported more than 80 countries to
+          {' '}
+          <strong>promote trade in line with environmental, social and economic criteria</strong>
+          .
+        </p>
+        <p>
+          The initiative provides a set of
+          {' '}
+          <a href="/topic/trade-and-environment/biotrade/principles-and-criteria">guidelines</a>
+          {' '}
+          to bolster sustainable development in its three dimensions across the entire value chain.
+        </p>
+        <p>
+          These entail a dual focus on people and biodiversity, spotlighting core elements such as
+          {' '}
+          <strong>conservation, sustainable use, the fair and equitable sharing of benefits, and community empowerment.</strong>
+        </p>
+        <p>Equally important, meeting participants said, are the need for collaborative action, the promotion of sustainable livelihoods and legal compliance.</p>
+        <p>“We need to mobilize stakeholders at different levels and move the needle on reversing trends that are eroding the very basis of our human existence”, said Monica Rubiolo, head of trade promotion at the Swiss State Secretariat for Economic Affairs (SECO), which has supported the BioTrade initiative for almost two decades.</p>
+        <h4>Win-win for people, planet and economies</h4>
+        <p>Biodiversity is the natural capital base for a sustainable economy. By aligning with the BioTrade principles and criteria, developing countries can diversify exports, conserve biodiversity and improve the livelihoods of local communities.</p>
+        <figure>
+          <img src="https://dummyimage.com/1600x900/000/fff" alt="Main" />
+          <figcaption>
+            <strong>© Shutterstock/Somchai_Stock</strong>
+            {' '}
+            | A coffee farmer in Gia Lai, Vietnam. Sustainable trade helps conserve natural resources and empower communities.
+          </figcaption>
+        </figure>
+        <p>Take South Africa for example, where efforts to implement the initiative have created over 3,700 jobs, boosted local sales by 51%, and increased export sales by 178%.</p>
+        <p>In Peru, trade of native superfoods with high nutrition, including quinoa and maca, generate around $500 million annually.</p>
+        <p>Additionally, Indonesia saw exports of biodiversity-based products soar by a third in 2021 alone, representing more than 36% of the country’s total exports, and close to 10% of its GDP.</p>
+        <h4>How BioTrade Congress makes a difference</h4>
+        <p>The congress series, first created in 2012, helps mainstream biodiversity into global trade discussions among governments, businesses, researchers and civil society representatives.</p>
+        <p>
+          The meeting is also part of wider activities towards the 16th meeting of the Conference of the Parties to the
+          {' '}
+          <a href="https://www.un.org/en/observances/biological-diversity-day/convention">UN Convention on Biological Diversity</a>
+          , as well as UNCTAD’s
+          {' '}
+          <a href="/news/unctad-mark-60th-anniversary-global-leaders-forum">60th anniversary</a>
+          {' '}
+          (2024) and its 16th quadrennial conference, set for 2025.
+        </p>
+        <p>&quot;The Bio Trade Congress reminds us of the importance of putting people, societies and the environment before capital, by looking more closely at social and solidarity economy models to advance sustainable livelihoods,&quot; said Chantal Line Carpentier, head of trade, environment, climate and sustainable development at UNCTAD.</p>
+        <p>
+          Deliberations from the congress will support the implementation of the
+          {' '}
+          <a href="https://www.un.org/sustainabledevelopment/sustainable-development-goals/">Sustainable Development Goals</a>
+          , and the landmark
+          {' '}
+          <a href="/topic/trade-and-environment/biotrade/kunming-montreal-global-biodiversity-framework">Kunming-Montreal Global Biodiversity Framework</a>
+          , aimed at halting and reversing biodiversity loss by 2030.
+        </p>
+      </div>
     </div>
 
   );
